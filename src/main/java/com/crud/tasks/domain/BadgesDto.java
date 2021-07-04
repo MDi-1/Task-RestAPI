f441c284ef583680c71e6f80cf3dd5615cc7091d
MDi <calendarzyk@gmail.com>
2021-07-04 15:54:58 +0200

    tasks module 22.3 in progress

--- src/main/java/com/crud/tasks/domain/BadgesDto.java
+++ src/main/java/com/crud/tasks/domain/BadgesDto.java
@@ -3,18 +3,12 @@ package com.crud.tasks.domain;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-import java.util.List;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TrelloBoardDto {
+public class BadgesDto {
 
-    @JsonProperty("id")
-    private String id;
-
-    @JsonProperty("name")
-    private String name;
-
-    @JsonProperty("lists")
-    private List<TrelloListDto> lists;
+    @JsonProperty("votes")
+    private int votes;
+    private AttachmentsByTypeDto attachmentsByTypeDto;
 }
