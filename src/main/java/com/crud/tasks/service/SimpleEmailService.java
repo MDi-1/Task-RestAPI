97672ea3e13e5ee8338d375831d2e5fc740e3229
MDi <calendarzyk@gmail.com>
2021-07-28 21:37:18 +0200

    tasks module 32.2 in progress

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -4,9 +4,12 @@ import com.crud.tasks.domain.Mail;
 import com.sun.source.tree.OpensTree;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -17,6 +20,9 @@ import java.util.Optional;
 public class SimpleEmailService {
 
     private final JavaMailSender javaMailSender;
+
+    @Autowired
+    private MailCreatorService mailCreatorService;
 
     public void send(final Mail mail) {
         log.info("Starting email preparation...");
@@ -41,6 +47,23 @@ public class SimpleEmailService {
         } else {
             System.out.println("\nMail class attribute CC is empty\n");
         }
+        return mailMessage;
+    }
+
+    private MimeMessagePreparator createMimeMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
+    }
+
+    private SimpleMailMessage createMailMessage(final Mail mail) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()));
         return mailMessage;
     }
 }
